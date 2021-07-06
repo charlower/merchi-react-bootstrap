34d66ffc96901d45e7092b20c5d9ee3951e9ea4f
Charlie <charlows@gmail.com>
2021-07-06 15:06:06 +1000

    boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="vh-100 d-flex justify-content-center align-items-center">
+      <div className="d-flex flex-column align-items-center">
+        <h1 className="">Merchi React Bootstrap</h1>
+        <p className="">
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
