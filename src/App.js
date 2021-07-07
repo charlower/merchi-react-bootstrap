c2fc1385ea5c8f785f073faa993737b5f6f42cfc
Charlie <charlows@gmail.com>
2021-07-07 15:55:59 +1000

    updated App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
+import React, { useState, useEffect } from "react";
+
 // Manually initialising merchi object like so:
 const merchi = window.SDK.merchi(
   "https://api.merchi.co/",
   "https://websockets.merchi.co/"
 );
-// Example fetching some products
-function onSuccess(products) {
-  console.log("Ok, got some products!");
-  console.log(products.length);
-  for (const product of products) {
-    console.log(product);
-  }
-}
-
-const onError = console.error.bind(console);
-
-const parameters = { inDomain: 103 }; // inDomain accepts a domain ID to filter products by domain
-
-merchi.products.get(onSuccess, onError, parameters);
 
 function App() {
+  const [productsState, setProductsState] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(async () => {
+    await merchi.products.get(onSuccess, onError, parameters); // Fetch Merchi producs
+  }, []);
+
+  function onSuccess(products) {
+    setProductsState(products);
+    setLoading(false);
+  }
+
+  const onError = console.error.bind(console);
+
+  const parameters = { inDomain: 103 }; // inDomain accepts a domain ID to filter products by domain
+
   return (
     <div className="vh-100 d-flex justify-content-center align-items-center">
       <div className="d-flex flex-column align-items-center">
@@ -26,6 +30,15 @@ function App() {
         <p className="">
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {loading ? (
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        ) : (
+          productsState.map((item, i) => {
+            return <li key={i}>{item.name()}</li>;
+          })
+        )}
       </div>
     </div>
   );
